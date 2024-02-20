47f82feca6b60c117573331de588e8e3ea338df4
Tamer Ahmed <tamer30230@gmail.com>
2024-02-20 15:01:24 +1100

    update server file

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use('/api/categories', categoriesRouter);
 
 if(process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     app.get('*', (req, res) => 
-        res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '/frontend/build', 'index.html'))
     );
 } else {
     app.get('/', (req, res)=> {
